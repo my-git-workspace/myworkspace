b43d1496cd9f064b45c4b3e2a0c6a0767c5fbf18
somnath.ghule <somnath.ghule@msystechnologies.com>
2017-10-25 23:04:06 +0530

    modified:   sample_driver.c

--- programming/kernel-programming/writing-device-drivers/sample_driver/sample_driver.c
+++ programming/kernel-programming/writing-device-drivers/sample_driver/sample_driver.c
@@ -1,10 +1,24 @@
 #ifdef __KERNEL__
 #include <linux/module.h>
 #include <linux/fs.h>
+#include <linux/cdev.h>
 
 dev_t first;
 unsigned int count=0;
 char *dev_name="mchardev";
+struct cdev *mycdev;
+int cdev_open(struct inode *ip, struct file *fp){
+	printk(KERN_INFO "%s\n",__func__);
+    return 0;
+}
+int cdev_release(struct inode *ip, struct file *fp){
+	printk(KERN_INFO "%s\n",__func__);
+    return 0;
+}
+struct file_operations fops={
+	open:cdev_open,
+	release:cdev_release   
+};
 int init_char_dev(void){
 	int err=0;
 	err=register_chrdev_region(first,count,dev_name);
@@ -12,6 +26,12 @@ int init_char_dev(void){
 	    return err;
 	}
 	printk(KERN_INFO "register_chrdev_region: success\n");
+	err=alloc_chrdev_region(&first,0,count,dev_name);
+	mycdev=cdev_alloc();	
+	cdev_init(mycdev,&fops);
+	cdev_add(mycdev, first,count);
+	printk(KERN_INFO "Major: %d\n",MAJOR(first));
+	printk(KERN_INFO "Minor: %d\n",MINOR(first));
 	return err;
 }
 static int init_sample_driver(void){
@@ -23,6 +43,7 @@ static int init_sample_driver(void){
 }
 int free_char_dev(void){
 	int err=0;
+	cdev_del(mycdev);
 	unregister_chrdev_region(first,count);
 	printk(KERN_INFO "unregister_chrdev_region: success\n");
 	return err;
@@ -38,8 +59,15 @@ module_exit(exit_sample_driver);
 #else /* !KERNEL */
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
 int main(int argc, char **argv[]){
 	printf("This is userland\n");
+	int fd=open("/home/msys/myworkspace/programming/kernel-programming/writing-device-drivers/sample_driver/mycdev",O_RDONLY);
+	if(fd){
+		printf("device opening failed\n");
+		perror("open mycdev failed");
+	}
+	close(fd);
 	//return from main
 	return (0);
 }
