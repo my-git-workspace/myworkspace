89abebd9d55043facae8f0f050ee02e9b080b637
Somnath Ghule <somnathbghule@gmail.com>
2019-06-10 09:42:25 +0530

    atomic ops added.

--- programming/multithreading/producer_consumer/producer_consumer.c
+++ programming/multithreading/producer_consumer/producer_consumer.c
@@ -4,11 +4,12 @@
 #include <unistd.h>
 #include <semaphore.h>
 #include "queue.h"
+#include "atomic.h"
 
-#define TOTAL_ELEMENT 2000
+#define TOTAL_ELEMENT 10
 #define BUF_SIZE MAX
 int produce_count=0;
-int consumer_count=TOTAL_ELEMENT;
+atomic_t consumer_count=ATOMIC_INIT(TOTAL_ELEMENT + 1);
 int buffer[BUF_SIZE];
 int i = 0;
 sem_t sem_full;
@@ -18,35 +19,44 @@ void *produce(void *param){
 	static int data;
 	while ( produce_count < TOTAL_ELEMENT ) {
 		sem_wait(&sem_empty);
+		printf("tid %lu: ", pthread_self());
 		put(data++);
 		produce_count++;
 		sem_post(&sem_full);
 	}
+	return NULL;
 }
 void *consume(void *param){
-	while ( consumer_count  ) {
+	while ( atomic_read(&consumer_count) ) {
 		sem_wait(&sem_full);
+		printf("tid %lu: ", pthread_self());
 		get();
-		consumer_count--;
+		atomic_dec(&consumer_count);
 		sem_post(&sem_empty);
 	}
+	return NULL;
 }
 
 int main(int argc, char **argv){
-	pthread_t producer, consumer;
+	pthread_t producer, consumer[2];
 	int ret=0;
 	sem_init(&sem_full, 0, 0);
 	sem_init(&sem_empty, 0, BUF_SIZE);
 	ret=pthread_create(&producer, NULL, produce, NULL);
 	if( ret ){
-		printf("pthread 1 create failed");	
+		printf("producer create failed");	
 	}
-	ret=pthread_create(&consumer, NULL, consume, NULL);
+	ret=pthread_create(&consumer[0], NULL, consume, NULL);
 	if( ret ){
-		printf("pthread 2 create failed");	
+		printf("consumer [0] create failed");	
+	}
+	ret=pthread_create(&consumer[1], NULL, consume, NULL);
+	if( ret ){
+		printf("consumer [1] create failed");	
 	}
 	pthread_join(producer, NULL);
-	pthread_join(consumer, NULL);
+	pthread_join(consumer[0], NULL);
+	pthread_join(consumer[1], NULL);
 	//return 
 	return (0);
 }
