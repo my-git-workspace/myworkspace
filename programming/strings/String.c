7a53443d0bee5e18655884c2a3fbed9adf824f4a
Somnath Ghule <somnathbghule@gmail.com>
2019-09-03 17:43:25 +0530

    solution updated

--- programming/strings/String.c
+++ programming/strings/String.c
@@ -11,11 +11,42 @@ int strLen(const char *str) {
 	return len;
 }
 
-int maxLengthEvenWord(const char *string, char *maxLengthWord) {
-	return 0;
+#define IS_CHAR_SPACE(c) (c==' ')
+#define IS_CHAR_NULLTERM(c) (c=='\0')
+
+int maxLengthEvenWord(const char *string, char *maxEvenLengthWord) {
+	char c='\0';
+	char wordBegin=0;
+	int maxEvenLen=0;
+	int charCount=0;
+	int i=0;
+	while((c=*(string + i))){
+		if( !wordBegin && !IS_CHAR_SPACE(c) ){
+			wordBegin=1;
+			charCount++;
+		}else if ( wordBegin && !IS_CHAR_SPACE(c) && !IS_CHAR_NULLTERM(*(string+i+1)) ){
+			charCount++;
+		}else if ( IS_CHAR_SPACE(c) || IS_CHAR_NULLTERM(*(string+i+1))){
+			if(IS_CHAR_NULLTERM(*(string+i+1))){
+				charCount++;
+				i++;
+			}
+			if ( charCount % 2 == 0 && charCount > maxEvenLen ){
+				maxEvenLen = charCount;	
+				strncpy(maxEvenLengthWord, string + i - charCount,charCount);
+				maxEvenLengthWord[charCount]='\0';
+			}
+			charCount=0;
+			wordBegin = 0;
+		}	
+		i++;
+	}
+	printf("maxLengthEvenWord(\"%s\") = \"%s\"\n", string,maxEvenLengthWord);
+	return charCount;
 }
 
 int main(int argc, char **argv){
-	strLen("Somnath");
+	char buf[32];
+	maxLengthEvenWord("Hello Hi 12345678 hell", buf);
 	return 0;
 }
