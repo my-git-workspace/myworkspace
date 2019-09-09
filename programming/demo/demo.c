3da9d1b803348702209616d84244c596ed85b492
Somnath Ghule <somnathbghule@gmail.com>
2019-09-09 10:43:28 +0530

    demo.c updated.

--- programming/demo/demo.c
+++ programming/demo/demo.c
@@ -11,8 +11,17 @@
 
 
 int main(int argc, char **argv){
+	
 
 	printf("%s\n", STR);
+
+
+	int vSize = 64;
+	const int fSize = 129;
+	if ( ( fSize > vSize ) && ( fSize%vSize == 1 ) ){
+		printf("update adress\n");
+	}
+
 	//return from main
 	return (0);
 }
